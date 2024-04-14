20af3d66f648c2db160683891c0560d2b80b62f5
Your Name <you@example.com>
2024-04-15 00:44:27 +0600

    githut is added in login page

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -17,7 +17,7 @@ const PrivateRoute = ({children}) =>{
         
     }
 
-    return <Navigate state={location.pathname} to='/login'></Navigate>
+    return <Navigate state={location.pathname} to='/yourAccountLogin'></Navigate>
 };
 
 export default PrivateRoute;
